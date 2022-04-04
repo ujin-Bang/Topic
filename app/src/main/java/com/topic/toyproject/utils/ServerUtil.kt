f6576416dd2a6cb86b896d22e77b3dd8233b86a3
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-04 20:36:59 +0900

    로그인 실패시에만, message문구 파싱/ 로그

--- app/src/main/java/com/topic/toyproject/utils/ServerUtil.kt
+++ app/src/main/java/com/topic/toyproject/utils/ServerUtil.kt
@@ -75,6 +75,9 @@ class ServerUtil {
                     }
                     else{
                         Log.d("로그인시도","실패!")
+
+                        val message = jsonObj.getString("message")
+                        Log.d("실패사유",message)
                     }
 
                 }
