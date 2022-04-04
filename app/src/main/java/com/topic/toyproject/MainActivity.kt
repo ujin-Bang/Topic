e814b43bb201ba12beb8a022a26c2affb68098d4
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-04 10:26:37 +0900

    로그인 기능 수행 함수 제작 중  - 별도 클래스 생성(ServerUtil)  - companion objet로 static과 유사한 효과  - 로그인 담당 함수 > url정리

--- app/src/main/java/com/topic/toyproject/MainActivity.kt
+++ app/src/main/java/com/topic/toyproject/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.topic.toyproject.databinding.ActivityMainBinding
+import com.topic.toyproject.utils.ServerUtil
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +24,8 @@ class MainActivity : AppCompatActivity() {
             val inputPw = binding.edtPassword.text.toString()
             
 //            API서버에 아이디 / 비번을 보내서 실제로 회원인지 검사 -> 로그인 시도
+
+
         }
         
     }
---
+++ app/src/main/java/com/topic/toyproject/utils/ServerUtil.kt
@@ -0,0 +1,25 @@
+package com.topic.toyproject.utils
+
+class ServerUtil {
+
+//    서버에 Request를 날리는 역할
+//    함수를 만들려고 하는데 어떤 객체가 실행해도 결과만 잘 나오면 그만인 함수.
+//    코틀린에서 static에 해당하는 개념? companion object{ }에 만들자.
+
+    companion object {
+//        서버 컴퓨터 주소만 변수로 저장(관리 일원화) => 외부노출x
+        private val BASE_URL = "http://54.180.52.26"
+
+//        로그인 기능 호출 함수.
+        fun postRequestLogin( id: String, pw: String){
+
+//            Request제작 -> 실제 호출 ->서버의 응답을 화면에 전달.
+
+//            제작1) 어느주소(url)로 접근할지?=> 서버주소 + 기능 주소
+            val urlString = "${BASE_URL}/user"
+
+        }
+
+    }
+
+}
