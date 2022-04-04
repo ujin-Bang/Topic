3305b6c323731c8426e21ef37c18c89fb06e0a41
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-04 21:16:40 +0900

    서버통신이 완료된 후의 처리를 화면단에서 코딩  - 토스트를 띄우려니 앱이 죽는다

--- app/src/main/java/com/topic/toyproject/MainActivity.kt
+++ app/src/main/java/com/topic/toyproject/MainActivity.kt
@@ -2,9 +2,11 @@ package com.topic.toyproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.topic.toyproject.databinding.ActivityMainBinding
 import com.topic.toyproject.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,7 +26,20 @@ class MainActivity : AppCompatActivity() {
             val inputPw = binding.edtPassword.text.toString()
             
 //            API서버에 아이디 / 비번을 보내서 실제로 회원인지 검사 -> 로그인 시도
-            ServerUtil.postRequestLogin(inputId, inputPw)
+            ServerUtil.postRequestLogin(inputId, inputPw, object :ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+//                    화면의 입장에서 로그인 결과를 받아서 처리할 코드
+                    val code = jsonObj.getInt("code")
+
+                    if (code == 200 ){
+
+                    }
+                    else{
+                        Toast.makeText(this@MainActivity, "로그인 실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            })
 
         }
         
