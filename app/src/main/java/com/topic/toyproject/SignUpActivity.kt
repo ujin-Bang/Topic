021b64a9228e7483ac3d2e07395728ce4e9cb2a3
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-05 10:51:38 +0900

    추상메쏘드 2개를 물려주는 클래스 생성  - 추상 클래스로 생성(xml은 만들 필요 X)

--- app/src/main/java/com/topic/toyproject/SignUpActivity.kt
+++ app/src/main/java/com/topic/toyproject/SignUpActivity.kt
@@ -12,14 +12,7 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
-        setupEvents()
-        setValues()
-    }
-
-    fun setupEvents(){
 
     }
-    fun setValues(){
 
-    }
 }
