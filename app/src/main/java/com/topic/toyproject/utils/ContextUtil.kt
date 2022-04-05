0a0fc6fb605c0f27fdcf540f538ca0f0dab8164b
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-05 18:36:55 +0900

    토큰 항목 변수 생성 / setter함수 제작

--- app/src/main/java/com/topic/toyproject/utils/ContextUtil.kt
+++ app/src/main/java/com/topic/toyproject/utils/ContextUtil.kt
@@ -1,5 +1,7 @@
 package com.topic.toyproject.utils
 
+import android.content.Context
+
 class ContextUtil {
 
     companion object {
@@ -7,6 +9,22 @@ class ContextUtil {
 //  메모장 파일 이름처럼, SharedPreferences의 이름 설정.
 
         private val prefName = "OkHttpPracticePref"
+
+//        저장할 데이터의 항목명도 변수로 만들어 두자.
+
+        val TOKEN = "TOKEN"
+
+//        데이터 저장 함수(setter) / 조회함수(getter) 별개로 작성
+//        TOKEN 항목에 저장 => token 항목 조회? 데이터 인식x. 대소문자까지 동일해야 함
+//        오타를 줄이고, 코딩 편하게 하는 조치로 항목명을 변수로 만듬
+
+        fun setToken( context: Context, token: String ){
+
+//            메모장 파일을 열자.
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+//            입력 들어온 token 내용(TOKEN 항목에) 저장.
+            pref.edit().putString(TOKEN, token).apply()
+        }
 
 
     }
