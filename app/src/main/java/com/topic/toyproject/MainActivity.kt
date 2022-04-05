a03be25774b57064f4d82568bb5fa2d1f40ab74c
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-05 14:17:41 +0900

    BaseActivity 상속 처리  - 모든 액티비티에 이 세팅도 진행 예정

--- app/src/main/java/com/topic/toyproject/MainActivity.kt
+++ app/src/main/java/com/topic/toyproject/MainActivity.kt
@@ -5,12 +5,23 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.topic.toyproject.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
 }
