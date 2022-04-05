8892524b07e56e761a0d610f8556df4387e09fe8
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-05 17:28:05 +0900

    이메일 / 닉네임 중복검사 맞나?

--- app/src/main/java/com/topic/toyproject/SignUpActivity.kt
+++ app/src/main/java/com/topic/toyproject/SignUpActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract.DisplayNameSources.EMAIL
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
@@ -17,38 +18,40 @@ class SignUpActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
         setupEvents()
         setValues()
     }
+
     override fun setupEvents() {
 
         binding.edtNickname.addTextChangedListener {
+//       닉네임 입력값 변경시 재검사 문구로 바꾸기
             binding.txtNicknameCheckResult.text = "중복 확인을 해주세요."
         }
 //        닉네임 검사 버튼 기능
-
-
         binding.btnNicknameCheck.setOnClickListener {
 
             val inputNickname = binding.edtNickname.text.toString()
 
-            ServerUtil.getRequestDuplicatedCheck("NICK_NAME", inputNickname, object : ServerUtil.JsonResponseHandler{
-                override fun onResponse(jsonObj: JSONObject) {
-                    val code = jsonObj.getInt("code")
-                    runOnUiThread {
+            ServerUtil.getRequestDuplicatedCheck(
+                "NICK_NAME",
+                inputNickname,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(jsonObj: JSONObject) {
+                        val code = jsonObj.getInt("code")
+                        runOnUiThread {
 
-                        if(code == 200){
-                            binding.txtNicknameCheckResult.text = "사용해도 좋은 닉네임입니다."
+                            if (code == 200) {
+                                binding.txtNicknameCheckResult.text = "사용해도 좋은 닉네임입니다."
+                            } else {
+                                binding.txtNicknameCheckResult.text = "다른 닉네임으로 중복확인해주세요."
+                            }
                         }
-                        else {
-                            binding.txtNicknameCheckResult.text = "다른 닉네임으로 중복확인해주세요."
-                        }
+
                     }
 
-                }
-
-            })
+                })
 
         }
 
@@ -66,84 +69,117 @@ class SignUpActivity : BaseActivity() {
 //            서버에 중복확인 기능(/user_check - GET) API활용 => ServerUtil에 함수 추가, 가져다 활용
 //            그 응답 code값에 따라 다른 문구 배치
 
-            ServerUtil.getRequestDuplicatedCheck("EMAIL", inputEmail, object : ServerUtil.JsonResponseHandler{
-                override fun onResponse(jsonObj: JSONObject) {
+            ServerUtil.getRequestDuplicatedCheck(
+                "EMAIL",
+                inputEmail,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(jsonObj: JSONObject) {
 
 //              code값에 따라 이메일 사용 가능 여부
-                    val code = jsonObj.getInt("code")
+                        val code = jsonObj.getInt("code")
 
-                    runOnUiThread{
-                        when(code) {
-                            200 -> {
-                                binding.txtEmailCheckResult.text ="사용해도 좋은 이메일입니다."
-                            }
-                            else ->{
-                                binding.txtEmailCheckResult.text = "다른 이메일로 다시 검사해주세요."
-                            }
+                        runOnUiThread {
+                            when (code) {
+                                200 -> {
+                                    binding.txtEmailCheckResult.text = "사용해도 좋은 이메일입니다."
+                                }
+                                else -> {
+                                    binding.txtEmailCheckResult.text = "다른 이메일로 다시 검사해주세요."
+                                }
 
+                            }
                         }
+
+
                     }
 
-
-
-
-                }
-
-            })
+                })
 
         }
 
+
+
         binding.btnSignUp.setOnClickListener {
+
+//            만약에 이메일 / 닉네임 중복검사를 통과하지 못한 상태라면
+//            토스트로 "이메일 중복검사를 통과해야 합니다." 등의 문구만 출력, 가입진행 X
+
+//            hint) 진행할 상황이 아니라면, return처리하면 함수 종료
+
+            if (!(binding.txtEmailCheckResult.text == "사용해도 좋은 이메일입니다.")) {
+                Toast.makeText(mContext, "이메일 중복검사를 통과해야합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (!(binding.txtNicknameCheckResult.text == "사용해도 좋은 닉네임입니다.")) {
+                Toast.makeText(mContext, "닉네임 중복검사를 통과해야합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
             val inputEmail = binding.edtEmail.text.toString()
             val inputPw = binding.edtPassword.text.toString()
             val inputNickname = binding.edtNickname.text.toString()
 
-            ServerUtil.putRequestSignUp(
-                inputEmail,
-                inputPw,
-                inputNickname,
-                object : ServerUtil.JsonResponseHandler{
-                    override fun onResponse(jsonObj: JSONObject) {
+
+            if (inputPw.isEmpty()|| inputPw.length < 7) {
+                Toast.makeText(mContext, "비밀번호를 입력해주세요.(7글자이상)", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+                ServerUtil.putRequestSignUp(
+                    inputEmail,
+                    inputPw,
+                    inputNickname,
+                    object : ServerUtil.JsonResponseHandler {
+                        override fun onResponse(jsonObj: JSONObject) {
 
 //                        회원가입 성공/ 실패 분기
-                        val code = jsonObj.getInt("code")
+                            val code = jsonObj.getInt("code")
 
-                        if (code == 200){
+                            if (code == 200) {
 
 //                            가입한 사람의 닉네임 추출 -> ~~님 가입을 축하합니다. 토스트
-                            val dataObj = jsonObj.getJSONObject("data")
-                            val userObj = dataObj.getJSONObject("user")
-                            val nickname = userObj.getString("nick_name")
+                                val dataObj = jsonObj.getJSONObject("data")
+                                val userObj = dataObj.getJSONObject("user")
+                                val nickname = userObj.getString("nick_name")
 
-                            runOnUiThread {
-                                Toast.makeText(mContext, "${nickname}님 가입을 축하합니다.", Toast.LENGTH_SHORT).show()
+                                runOnUiThread {
+                                    Toast.makeText(
+                                        mContext,
+                                        "${nickname}님 가입을 축하합니다.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
 
-                            }
+                                }
 //                            회원가입 화면 종료 >로그인화면 복귀
 //                            화면 종료: 객체 소멸(UI동작이 아님)
-                            finish()
-                        }
-                        else {
-                            val message = jsonObj.getString("message")
+                                finish()
+                            } else {
+                                val message = jsonObj.getString("message")
 
-                            runOnUiThread {
-                                Toast.makeText(mContext, "실패사유 : ${message}", Toast.LENGTH_SHORT).show()
+                                runOnUiThread {
+                                    Toast.makeText(
+                                        mContext,
+                                        "실패사유 : ${message}",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
 
+                                }
                             }
+
                         }
 
-                    }
+                    })
+            }
 
-                })
         }
 
-    }
 
-    override fun setValues() {
+        override fun setValues() {
 
-    }
-
-
-
+        }
 }
+
+
+
