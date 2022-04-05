abbb6e3e634b19a0d4dca77dfd14bee7bda74b00
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-05 16:12:40 +0900

    닉네임 중복검사 기능 구현  - 이미 만들어져 있는 함수 재활용

--- app/src/main/java/com/topic/toyproject/SignUpActivity.kt
+++ app/src/main/java/com/topic/toyproject/SignUpActivity.kt
@@ -23,6 +23,35 @@ class SignUpActivity : BaseActivity() {
     }
     override fun setupEvents() {
 
+        binding.edtNickname.addTextChangedListener {
+            binding.txtNicknameCheckResult.text = "중복 확인을 해주세요."
+        }
+//        닉네임 검사 버튼 기능
+
+
+        binding.btnNicknameCheck.setOnClickListener {
+
+            val inputNickname = binding.edtNickname.text.toString()
+
+            ServerUtil.getRequestDuplicatedCheck("NICK_NAME", inputNickname, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+                    val code = jsonObj.getInt("code")
+                    runOnUiThread {
+
+                        if(code == 200){
+                            binding.txtNicknameCheckResult.text = "사용해도 좋은 닉네임입니다."
+                        }
+                        else {
+                            binding.txtNicknameCheckResult.text = "다른 닉네임으로 중복확인해주세요."
+                        }
+                    }
+
+                }
+
+            })
+
+        }
+
         binding.edtEmail.addTextChangedListener {
 
 //            내용이 한글자라도 바뀌면 무조건 재검사 요구 문장.
