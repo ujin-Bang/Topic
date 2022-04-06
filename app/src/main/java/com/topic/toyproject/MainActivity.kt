da4c069298f0b2e605123bb61dcf8b0150937c97
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-06 11:24:21 +0900

    토큰을 활용한 API통신 연습  - 서버 통신 함수의 재료로 Context를 추가로 받자.

--- app/src/main/java/com/topic/toyproject/MainActivity.kt
+++ app/src/main/java/com/topic/toyproject/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.topic.toyproject.databinding.ActivityMainBinding
+import com.topic.toyproject.utils.ServerUtil
 
 class MainActivity : BaseActivity() {
 
@@ -20,6 +21,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+//        화면의 텍스트뷰에 닉네임을 보여주기 위한 작업
+        ServerUtil.get
 
     }
 
