7bcb4bf1771366d53e88ef6c6f7d3795633dd8ff
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-06 17:28:35 +0900

    내 정보 가져오기를 시도 => 그 결과에 따라 토큰 유효 여부 검증

--- app/src/main/java/com/topic/toyproject/SplashActivity.kt
+++ app/src/main/java/com/topic/toyproject/SplashActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.topic.toyproject.utils.ContextUtil
+import com.topic.toyproject.utils.ServerUtil
+import org.json.JSONObject
 
 class SplashActivity : BaseActivity() {
 
@@ -26,14 +29,34 @@ class SplashActivity : BaseActivity() {
 
 //            자동로그인을 해도 되는가?
 //            1) 사용자가 자동로그인 의사를 OK 했는가?
-//            2) 로그인 시에 받아낸 토큰값이 지금도 유효한가?
-         if () {
+            val userAutoLogin = ContextUtil.getAutoLogin(mContext)
 
-//             둘다 OK라면 바로 메인화면으로 이동
-         }
-            else {
-//                아니라면 로그인 화면으로 이동
-         }
+//            2) 로그인 시에 받아낸 토큰값이 지금도 유효한가?
+            var isTokenOk = false //검사를 통과하면 true로 변경 예정
+
+//            2-1)저장된 토큰이 있는지?(임시 방편)
+//            2-2)그 토큰이 사용자 정보를 불러내는지?(실제)
+
+            ServerUtil.getRequestMyInfo(mContext,object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+//                    내 정보를 잘 가져왔다면? => code값을 200으로 내려준다
+
+//                    200이 아니라면? 무슨 이유던, 사용자 정보 로드 실패. (토큰 유효x)
+
+                    val code = jsonObj.getInt("code")
+                    isTokenOk = (code == 200)
+                }
+
+            })
+
+//         if () {
+//
+////             둘다 OK라면 바로 메인화면으로 이동
+//         }
+//            else {
+////                아니라면 로그인 화면으로 이동
+//         }
 
         }, 2500)
 
