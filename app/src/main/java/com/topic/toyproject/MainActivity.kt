729d9b7688976f121a57936a703a50d95ec7f1ce
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-06 15:55:47 +0900

    topics라는 JSONArray추출  -내부를 분석할 예정

--- app/src/main/java/com/topic/toyproject/MainActivity.kt
+++ app/src/main/java/com/topic/toyproject/MainActivity.kt
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.topic.toyproject.databinding.ActivityMainBinding
+import com.topic.toyproject.datas.TopicData
 import com.topic.toyproject.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+//    실제로 서버가 내려주는 주제 목록을 담을 그릇
+   val mTopicList = ArrayList<TopicData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,20 +26,27 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-//        화면의 텍스트뷰에 닉네임을 보여주기 위한 작업
-        ServerUtil.getRequestMyInfo(mContext, object :ServerUtil.JsonResponseHandler{
+
+//        메인 화면 정보 가져오기 => API호출 / 응답처리
+        getTopicListFromServer()
+
+    }
+
+    fun getTopicListFromServer(){
+
+        ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.JsonResponseHandler{
             override fun onResponse(jsonObj: JSONObject) {
 
+//                서버가 주는 토론 주제 목록 파싱 => mTopicList에 추가하기
                 val dataObj = jsonObj.getJSONObject("data")
-                val userObj = dataObj.getJSONObject("user")
-                val nickname = userObj.getString("nick_name")
+                val topicsArr = dataObj.getJSONArray("topics")
 
+//                topicsArr 내부를 하나씩 추출( JSONObject{ }) => TopicData()로 변환
 
             }
 
         })
 
     }
-
 
 }
