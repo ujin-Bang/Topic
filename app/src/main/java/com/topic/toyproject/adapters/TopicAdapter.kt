aabc99d0283ae23abed22a46ac30292fbec4c53b
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-06 15:45:32 +0900

    리스트뷰 어댑터 - getView함수 작성

--- app/src/main/java/com/topic/toyproject/adapters/TopicAdapter.kt
+++ app/src/main/java/com/topic/toyproject/adapters/TopicAdapter.kt
@@ -1,7 +1,11 @@
 package com.topic.toyproject.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.topic.toyproject.R
 import com.topic.toyproject.datas.TopicData
 
 class TopicAdapter(
@@ -9,4 +13,15 @@ class TopicAdapter(
     resId: Int,
     val mList: List<TopicData>
 ): ArrayAdapter<TopicData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.topic_list_item, null)
+
+        }
+        val row = tempRow!!
+
+        return row
+    }
 }
