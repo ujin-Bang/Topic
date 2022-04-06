db1d8308ed5b4f3142031b997740891e6d54ad9a
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-06 12:35:11 +0900

    토큰을 첨부해서 토큰에 맞는 내 정보 받아오기

--- app/src/main/java/com/topic/toyproject/MainActivity.kt
+++ app/src/main/java/com/topic/toyproject/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.topic.toyproject.databinding.ActivityMainBinding
 import com.topic.toyproject.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -22,7 +23,19 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 //        화면의 텍스트뷰에 닉네임을 보여주기 위한 작업
-        ServerUtil.get
+        ServerUtil.getRequestMyInfo(mContext, object :ServerUtil.JsonResponseHandler{
+            override fun onResponse(jsonObj: JSONObject) {
+
+                val dataObj = jsonObj.getJSONObject("data")
+                val userObj = dataObj.getJSONObject("user")
+                val nickname = userObj.getString("nick_name")
+
+                runOnUiThread {
+                    binding.txtLoginUserNickname.text = nickname
+                }
+            }
+
+        })
 
     }
 
