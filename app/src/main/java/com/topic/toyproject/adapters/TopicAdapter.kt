2237767ab75ef7bd4ff2833bdea2c78217605ea9
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-06 16:46:30 +0900

    리스트뷰의 실제 데이터 반영

--- app/src/main/java/com/topic/toyproject/adapters/TopicAdapter.kt
+++ app/src/main/java/com/topic/toyproject/adapters/TopicAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.topic.toyproject.R
 import com.topic.toyproject.datas.TopicData
 
@@ -21,6 +22,12 @@ class TopicAdapter(
 
         }
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val txtTitle = row.findViewById<TextView>(R.id.txtTitle)
+
+        txtTitle.text = data.title
 
         return row
     }
