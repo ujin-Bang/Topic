8470bff991b273f6163dd59e30a506a1f13ad330
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-06 17:19:06 +0900

    2.5초 뒤에 진행할 검사를 주석으로 남김

--- app/src/main/java/com/topic/toyproject/SplashActivity.kt
+++ app/src/main/java/com/topic/toyproject/SplashActivity.kt
@@ -2,6 +2,8 @@ package com.topic.toyproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : BaseActivity() {
 
@@ -16,6 +18,24 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        2.5초가 지나면 -> 자동로그인을 해도 되는지? -> 상황에 맞는 화면으로 이동.
+        val myHanler = Handler(Looper.getMainLooper())
+
+        myHanler.postDelayed({
+
+//            자동로그인을 해도 되는가?
+//            1) 사용자가 자동로그인 의사를 OK 했는가?
+//            2) 로그인 시에 받아낸 토큰값이 지금도 유효한가?
+         if () {
+
+//             둘다 OK라면 바로 메인화면으로 이동
+         }
+            else {
+//                아니라면 로그인 화면으로 이동
+         }
+
+        }, 2500)
 
     }
 }
