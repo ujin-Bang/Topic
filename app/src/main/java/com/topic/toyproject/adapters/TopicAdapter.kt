ab281637d2da06d11e35116fa813718ed28252cc
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-06 17:07:47 +0900

    Glide활용 토론 주제 이미지 표현

--- app/src/main/java/com/topic/toyproject/adapters/TopicAdapter.kt
+++ app/src/main/java/com/topic/toyproject/adapters/TopicAdapter.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.topic.toyproject.R
 import com.topic.toyproject.datas.TopicData
 
@@ -26,8 +28,15 @@ class TopicAdapter(
         val data = mList[position]
 
         val txtTitle = row.findViewById<TextView>(R.id.txtTitle)
+        val txtReplyCount = row.findViewById<TextView>(R.id.txtReplyCount)
+        val imgTopicBackground = row.findViewById<ImageView>(R.id.imgTopicBackground)
 
         txtTitle.text = data.title
+        txtReplyCount.text = "${data.replyCount.toString()} 명 참여중"
+//      data > 서버에서 준 주제 데이터
+//        imageURL 변수 파싱 => 이미지의 인터넷 주소.
+//        웹이 있는 이미지 > 이미지뷰에 적용 > Glide활용
+        Glide.with(mContext).load(data.imageURL).into(imgTopicBackground)
 
         return row
     }
