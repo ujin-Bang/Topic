c7a8d0e7fc0585877a8d9197d6776abf13edd4b2
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-06 15:37:08 +0900

    토론 주제 표현용 데이터 클래스 추가  - 생성자 X,멤버변수만 추가  - 서버의 데이터를 참고해서 추가

---
+++ app/src/main/java/com/topic/toyproject/datas/TopicData.kt
@@ -0,0 +1,10 @@
+package com.topic.toyproject.datas
+
+class TopicData {
+
+    var id = 0 // id는 Int라고 명시.
+    var title = "" //title은 String이라고 명시.
+    var imageURL = ""// 서버: img_url, 앱: imageURL 변수명 다르게 해봄
+    var replyCount = 0
+
+}
