b6f2a6e5811ac8003840145dc72e99d4e34999d9
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-07 15:58:18 +0900

    미리 파싱해둔 득표수도 표현하기

--- app/src/main/java/com/topic/toyproject/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/topic/toyproject/ViewTopicDetailActivity.kt
@@ -43,9 +43,13 @@ class ViewTopicDetailActivity : BaseActivity() {
         binding.txtTitle.text = mTopicData.title
         Glide.with(mContext).load(mTopicData.imageURL).into(binding.imgTopicBackground)
 
-//        1번진영 제목,, 2번진영 제목
+//        1번진영 제목, 2번진영 제목
         binding.txtSide01.text = mTopicData.sideList[0].title
         binding.txtSide02.text = mTopicData.sideList[1].title
+
+//        1번진영 득표수, 2번진영 득표수
+        binding.txtVoteCount1.text = "${mTopicData.sideList[0].voteCount}표"
+        binding.txtVoteCount2.text = "${mTopicData.sideList[1].voteCount}표"
 
     }
 
