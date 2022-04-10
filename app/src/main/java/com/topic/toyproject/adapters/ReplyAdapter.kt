4d2c4d2527281b8015dc0d11ba5e5f56659b44d4
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-04-10 16:44:31 +0900

    SimpleDateFormat 체험

--- app/src/main/java/com/topic/toyproject/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/topic/toyproject/adapters/ReplyAdapter.kt
@@ -11,6 +11,7 @@ import com.bumptech.glide.Glide
 import com.topic.toyproject.R
 import com.topic.toyproject.datas.ReplyData
 import com.topic.toyproject.datas.TopicData
+import java.text.SimpleDateFormat
 import java.util.*
 
 class ReplyAdapter(
@@ -40,7 +41,14 @@ class ReplyAdapter(
 
 //        임시 - 작성일자만 "2022-03-10" 형태로 표현 => 연/ 월/ 일 데이터로 가공
 //        월은 1작게 나옴 . +1로 보정
-        txtCreatedAt.text = "${data.createAt.get(Calendar.YEAR)}-${data.createAt.get(Calendar.MONTH)+1}-${data.createAt.get(Calendar.DAY_OF_MONTH)}"
+//        txtCreatedAt.text = "${data.createAt.get(Calendar.YEAR)}-${data.createAt.get(Calendar.MONTH)+1}-${data.createAt.get(Calendar.DAY_OF_MONTH)}"
+
+//        임시 2 - "2022-03-10" 형태로 표현 => SimpleDateFormat 활용
+        val sdf = SimpleDateFormat("yyyy-MM-dd a hh시 mm분 ss초" )
+
+//        sdf.format(Date객체) => 지정해둔 양식의 String으로 가공
+//        createdAt : Calendar / format의 파라미터 : Date => Calendar의 내용물인 time변수가 Date.
+        txtCreatedAt.text = sdf.format(data.createAt.time)
 
         return row
     }
